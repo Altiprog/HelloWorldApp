fe4dbf859345a922d2ae4779916622fef35e4f48
Altiprog <atiunovich@gmail.com>
2023-09-25 14:05:00 +0200

    ib

--- HelloWorldApp/ViewController.swift
+++ HelloWorldApp/ViewController.swift
@@ -9,11 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloLable: UILabel!
+    
+    @IBOutlet weak var buttonOne: UIButton!
+    
+    @IBOutlet weak var buttonTwo: UIButton!
+    
+    @IBOutlet weak var buttonTree: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func buttonOnePressed(_ sender: UIButton) {
+//        if sender == buttonOne {
+//            helloLable.text = "one"
+//        } else if sender == buttonTwo {
+//            helloLable.text = "two"
+//        } else if sender == buttonTree {
+//            helloLable.text = "three"
+//        }
+       
+//        switch sender {
+//        case buttonOne:
+//            helloLable.text = "one"
+//        case buttonTwo:
+//            helloLable.text = "Two"
+//        default:
+//            helloLable.text = "Three"
+//            
+            
+            
+        }
+        
+        
+        
+    }
+    
+    
+    
 
-}
+
 
